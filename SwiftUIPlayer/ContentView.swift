f9b8b306bb8a521080eba8afa5d76f52a5e73510
Lex <lex.hrabovskyi@gmail.com>
2019-12-29 21:57:24 +0600

    Initial Commit

---
+++ SwiftUIPlayer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIPlayer
+//
+//  Created by Александр on 29.12.2019.
+//  Copyright © 2019 Александр. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
